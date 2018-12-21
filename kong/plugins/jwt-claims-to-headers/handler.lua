9997118339aa9be5e34cf04b48dcca4b6f95c5a1
Tova Roth <tova@us.ibm.com>
2018-12-21 14:12:39 -0500

    Parse the jwt, extract claims and add headers in the 'access' method

--- kong/plugins/jwt-claims-to-headers/handler.lua
+++ kong/plugins/jwt-claims-to-headers/handler.lua
@@ -1,6 +1,44 @@
 local BasePlugin = require "kong.plugins.base_plugin"
+local jwtParser = require "kong.plugins.jwt.jwt_parser"
 local JwtClaimsToHeadersHandler = BasePlugin:extend()
 
+-- ensure the priority is lower than the Jwt plugin, which has a priority of 1005
+JwtClaimsToHeadersHandler.PRIORITY = 10
+
+local JwtParamName = "jwt"
+
+local function extract_jwt(config)
+  local jwt
+  local err
+  local authHeader = kong.request.get_header("authorization")
+
+  if not authHeader == nil then
+    local bearer_pattern = "[Bb][Ee][Aa][Rr][Ee][Rr]"
+    jwt = authHeader.gsub(bearer_pattern, "")
+  end
+
+  if jwt == nil then
+    -- TODO: if there is a parameter that defines this name, then extract the jwt param name from the config, or default to 'jwt'
+    -- kong.log.inspect(config)
+    local jwt_param_name = JwtParamName
+    jwt = kong.request.get_query_arg(jwt_param_name)
+  end
+
+  -- TODO: error handling if no jwt is found - this may not necessary, since the 'jwt' plugin will return unauthorized before this is called
+  -- TODO: error handling if there is a jwt in the Bearer token and in a query parameter and they aren't the same:
+  -- TODO: return jwt, err. In the calling method, call: return kong.response.exit(400, "Bad Request: request should have a Bearer token or a jwt on a query parameter, not both")
+  -- TODO: see https://docs.konghq.com/0.14.x/pdk/kong.response/
+
+  return jwt, err
+end
+
+local function header_from_key(key_name, config)
+  -- TODO: check the config: first look for a mapping for the keys. If there is a mapping config, but none for this key, return nil
+  -- TODO: if there is no mapping, look for a prefix. If there is no prefix, use the default prefix
+
+  local prefix = "X-Jwt-Claim-"
+  return prefix .. key_name
+end
 
 function JwtClaimsToHeadersHandler:new()
   JwtClaimsToHeadersHandler.super.new(self, "jwt-claims-to-headers")
@@ -10,15 +48,44 @@ end
 function JwtClaimsToHeadersHandler:access(config)
   JwtClaimsToHeadersHandler.super.access(self)
 
-  ngx.log(ngx.ERR, "@@@@ Adding headers")
+  local jwt, err = extract_jwt(config)
+  -- TODO: handle a jwt that's nil
+  if (jwt == nil or jwt == '' or not (type(jwt) == "string")) then
+    kong.log.debug("Invalid jwt: type is ", type(jwt), ", jwt is ", jwt)
+  else
+    kong.log.debug("Jwt is : " .. jwt)
+
+    local jwt_table, err = jwtParser:new(jwt)
+    if err ~= nil then
+      -- TODO: handle an error
+      kong.log.error("Error parsing the jwt: ", err)
+    end
+
+    local claims = jwt_table["claims"]
+    kong.log.debug("Jwt claims:")
+    kong.log.inspect(claims)
+
+    if claims ~= nil then
+      for key, value in pairs(claims) do
+        -- TODO: create headers
+        kong.log.debug("@@@@@@ claim key is ", key, " key type is ", type(key), " value is ", value)
+        local header = header_from_key(key, config)
+        kong.log.debug("Set header: ", header, " value: ", value)
+        if header ~= nil then
+          kong.service.request.set_header(header, value)
+        end
+      end
+    end
+
+  end
+
   ngx.header["X-Jwt-ClaimX"] = "ClaimX value"
   ngx.req.set_header("X-Jwt-ClaimX", "ClaimX value")
   ngx.header["X-Jwt-ClaimY"] = "ClaimY value"
   ngx.req.set_header("X-Jwt-ClaimY", "ClaimY value")
 
-  
-  kong.log.inspect(config.key_names)   
-end
+  end
+
 
 
 return JwtClaimsToHeadersHandler
--- kong/plugins/jwt-claims-to-headers/schema.lua
+++ kong/plugins/jwt-claims-to-headers/schema.lua
@@ -2,6 +2,10 @@ return {
   no_consumer = true, -- this plugin will only be applied to Services or Routes,
   fields = {
     -- jwt_query_param_name: {type = "string", default = "jwt"}
+    -- TODO: optional parameter that has a map of each jwt claim to include as a header, and what to name the header.
+    -- TODO: optional parameter that has the prefix to use for defaulting the claim headers: if not map is given, use this prefix to map every claim.
+    -- TODO:  if neither a map nor a prefix is given, use a default prefix for all claims (X-Jwt-Claim-n)
+    -- TODO: parameter to specify the name of the 'jwt' parameter that the JWT plugin is configured with, for example 'jwt'. Is this necessary? Maybe it's confusing
   }
   -- ,
   -- self_check = function(schema, plugin_t, dao, is_updating)
